0267861491d45f5711775f032dcc6d514f6c5028
g-vasilei <ram.kunya.9@gmail.com>
2023-12-21 02:19:11 +0200

    add Q3

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,6 @@
 import Q1 from '../../public/imgs/quotes/quotes-1.png'
 import Q2 from '../../public/imgs/quotes/quotes-2.png'
+import Q3 from '../../public/imgs/quotes/quotes-3.png'
 
 const data = {
    projects: [
@@ -21,13 +22,13 @@ const data = {
          seoDescription: 'Project "I know that I m not that important to you.." | grumpy.gr',
       },
       {
-         title: 'Happy to be Sad',
+         title: 'Everybody so fake',
          description:
-            'This is how grumpy.gr was created. This moto inspired the creator for this sad website where he can share his thoughts',
-         slug: 'happy-to-be-sad',
-         img: Q1,
-         seoTitle: 'Project: Happy to be Sad | grumpy.gr',
-         seoDescription: '"Project Happy to be Sad.."',
+            'Everybody is so fake.. Sharing their life in social media just to show others the good life they are not living',
+         slug: 'everybody-so-fake',
+         img: Q3,
+         seoTitle: 'Project: Everybody so fake | grumpy.gr',
+         seoDescription: '"Project Everybody so fake.."',
       },
       {
          title: 'Happy to be Sad',
