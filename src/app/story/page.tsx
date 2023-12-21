cd4ea0bf4055d2806e474c979aee751036461a2d
g-vasilei <ram.kunya.9@gmail.com>
2023-12-21 02:03:23 +0200

    fix story pages 3

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -6,23 +6,23 @@ function page() {
          <p className='mb-10'>
             In a picturesque town in Greece, there resided a boy named George. He was a quiet soul, navigating a world
             that often felt overwhelming. Unlike most kids his age, George found solace in the haunting melodies and
-            melancholic lyrics of Lil Peep's songs. The raw emotion and honesty in the music resonated deeply with his
+            melancholic lyrics of Lil Peep s songs. The raw emotion and honesty in the music resonated deeply with his
             own feelings of sadness and isolation.
          </p>
          <p className='mb-10'>
             Growing up, George faced challenges fitting in with his peers. His sensitivity and introverted nature made
             it difficult for him to connect in a way that felt genuine. But amidst this struggle, he discovered a kind
-            of companionship in the music of Lil Peep. The lyrics seemed to articulate the emotions he couldn't express
+            of companionship in the music of Lil Peep. The lyrics seemed to articulate the emotions he couldn t express
             and provided a sense of validation for his own sadness.
          </p>
          <p className='mb-10'>
             As George journeyed through adolescence, the weight of his emotions grew heavier. He found himself turning
-            more to music, using it as a canvas to paint the colors of his emotions. Inspired by Lil Peep's honesty,
+            more to music, using it as a canvas to paint the colors of his emotions. Inspired by Lil Peep s honesty,
             George had an idea—a digital space where he could channel his emotions into something meaningful.
          </p>
          <p>
             He created a website, naming it
-            <span className='text-main-color font-bold text-lg tracking-tight'>GRUMPY</span> Here, George poured his
+            <span className='text-main-color font-bold text-lg tracking-tight'>"GRUMPY"</span> Here, George poured his
             heart out, sharing his thoughts, poetry, and art inspired by the songs that spoke to his soul. The site
             became a haven for those who resonated with the same emotions, drawing in individuals seeking solace in
             their shared love for music that captured the essence of their sadness.
