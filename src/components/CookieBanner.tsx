a6818a3cdb69c46a1408622b8bf84db6cb57e6da
g-vasilei <73238131+g-vasilei@users.noreply.github.com>
2023-12-23 13:47:28 +0200

    change cookie banner color

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -27,18 +27,26 @@ export default function CookieBanner() {
   }, [cookieConsent]);
 
   return (
+    // <div
+    //   className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+    //                   fixed bottom-0 left-0 right-0
+    //                   ${
+    //                     cookieConsent != null ? 'hidden' : 'flex'
+    //                   } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4
+    //                    bg-cookie-bg rounded-lg shadow `}
+    // >
     <div
       className={`my-10 mx-auto max-w-max md:max-w-screen-sm
                       fixed bottom-0 left-0 right-0 
                       ${
                         cookieConsent != null ? 'hidden' : 'flex'
                       } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                       bg-cookie-bg rounded-lg shadow `}
+                       bg-neutral-50 rounded-lg shadow `}
     >
-      <div className='text-center'>
+      <div className='text-center text-slate-950'>
         <Link href='/info/cookies'>
           <p>
-            We use <span className='font-bold text-sky-400'>cookies</span> on
+            We use <span className='font-bold text-main-color'>cookies</span> on
             our site.
           </p>
         </Link>
@@ -46,13 +54,13 @@ export default function CookieBanner() {
 
       <div className='flex gap-2'>
         <button
-          className='px-5 py-2 text-gray-300 rounded-md border-gray-900'
+          className='px-5 py-2 text-slate-950 rounded-md border-gray-900'
           onClick={() => setCookieConsent(false)}
         >
           Decline
         </button>
         <button
-          className='bg-slate-50 px-5 py-2 text-main-color font-medium rounded-lg'
+          className='bg-main-color px-5 py-2 text-white font-medium rounded-lg'
           onClick={() => setCookieConsent(true)}
         >
           Allow Cookies
