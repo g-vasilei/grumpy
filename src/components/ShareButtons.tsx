17ca52b50912ba02f6a5d0775203a84fc57c2f52
g-vasilei <ram.kunya.9@gmail.com>
2023-12-24 13:13:59 +0200

    fix share url in socials button

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -14,47 +14,47 @@ function ShareButtons({ slug }: { slug: string }) {
    return (
       <div className='flex items-center gap-5 flex-wrap mt-5'>
          <FacebookShareButton
-            url={`https://grumpy.gr//projects/${slug}`}
+            url={`https://grumpy.gr/projects/${slug}`}
             quote={'Sad and unmotivational quotes | grumpy.gr'}
             hashtag={'#grumpy.gr'}
          >
             <FacebookIcon size={32} round />
          </FacebookShareButton>
          <TwitterShareButton
-            url={'https://grumpy.gr//projects/${slug}'}
+            url={'https://grumpy.gr/projects/${slug}'}
             title={'Sad and unmotivational quotes | grumpy.gr'}
          >
             <TwitterIcon size={32} round />
          </TwitterShareButton>
          <RedditShareButton
-            url={'https://grumpy.gr//projects/${slug}'}
+            url={'https://grumpy.gr/projects/${slug}'}
             title={'Sad and unmotivational quotes | grumpy.gr'}
          >
             <RedditIcon size={32} round />
          </RedditShareButton>
          <TelegramShareButton
-            url={'https://grumpy.gr//projects/${slug}'}
+            url={'https://grumpy.gr/projects/${slug}'}
             title={'Sad and unmotivational quotes | grumpy.gr'}
          >
             <TelegramIcon size={32} round />
          </TelegramShareButton>
          <ViberShareButton
-            url={'https://grumpy.gr//projects/${slug}'}
+            url={'https://grumpy.gr/projects/${slug}'}
             title={'Sad and unmotivational quotes | grumpy.gr'}
          >
             <ViberIcon size={32} round />
          </ViberShareButton>
          <WhatsappShareButton
-            url={'https://grumpy.gr//projects/${slug}'}
+            url={'https://grumpy.gr/projects/${slug}'}
             title={'Sad and unmotivational quotes | grumpy.gr'}
             separator=':: '
          >
             <WhatsappIcon size={32} round />
          </WhatsappShareButton>
-         <FacebookMessengerShareButton url={'https://grumpy.gr//projects/${slug}'} appId={''}>
+         <FacebookMessengerShareButton url={'https://grumpy.gr/projects/${slug}'} appId={''}>
             <FacebookMessengerIcon size={32} round />
          </FacebookMessengerShareButton>
-         <EmailShareButton url={'https://grumpy.gr//projects/${slug}'} subject={'Next Share'} body='body'>
+         <EmailShareButton url={'https://grumpy.gr/projects/${slug}'} subject={'Next Share'} body='body'>
             <EmailIcon size={32} round />
          </EmailShareButton>
       </div>
